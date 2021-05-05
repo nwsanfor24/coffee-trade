7183b96ebf0269534f416573205c0ab66ec2815c
Nathan Sanford <nwsanfor24@gmail.com>
2021-05-05 16:35:20 -0400

    routes from Coffees and Coffee components work

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import Coffees from './Coffees/Coffees'
+import Coffee from './Coffee/Coffee'
 
 const App = () => {
     return (
